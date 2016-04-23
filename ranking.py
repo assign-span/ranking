a13772b5eb17d6b5f1c3d54c0e3e5652b8c7c2de
Tapiwa <assign-span@users.noreply.github.com>
2016-04-24 01:14:45 +0200

    renames main function

--- ranking.py
+++ ranking.py
@@ -1,7 +1,14 @@
 #!/bin/env python
 
-def main():
-    pass
+import sys
+def rank(input_file=sys.stdin, output_file=sys.stdout):
+    """
+    Process rankings
+    :param input_file: Optional file pointer to input file from; defaults to STDIN
+    :param output_file: Optional file pointer to output file; defaults to STDOUT
+    :return:
+    """
+    points = {}
 
 if __name__== "__main__":
-    main()
+    rank()
