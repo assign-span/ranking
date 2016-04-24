915338152d4a3c35e474c0e4189d9738a41c92e3
Tapiwa <assign-span@users.noreply.github.com>
2016-04-24 18:20:32 +0200

    Updates test cases in unit tests

--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -1,29 +1,5 @@
 from .. import ranking
-from mock import MagicMock
-
-def test_point_order():
-    """
-    Should return teams ranked by points
-    """
-    pass
-
-def test_tie():
-    """
-    Should show tied teams on the same position
-    """
-    assert 1 == 1, "tied are equal"
-
-def test_tie_order():
-    """
-    Should display tied teams in alphabetical order
-    """
-    pass
-
-def test_tie_trailing():
-    """
-    Should number teams lower than ties correctly
-    """
-    pass
+from mock import call, MagicMock
 
 
 def test_get_score():
@@ -60,3 +36,35 @@ def test_process_result():
 
     res = ranking.process_result("TeamA 1, TeamB 1")
     assert res == {"TeamA": 1, "TeamB": 1}
+
+
+def test__pluralize():
+    """
+    Test _pluralize()
+    """
+    res = ranking._pluralize(0)
+    assert res == "pts", "Expected 0 to be pluralized as 'pts', got {} instead".format(res)
+
+    res = ranking._pluralize(1)
+    assert res == "pt", "Expected 1 to be pluralized as 'pt', got {} instead".format(res)
+
+    res = ranking._pluralize(2)
+    assert res == "pts", "Expected 2 to be pluralized as 'pt', got {} instead".format(res)
+
+
+def test_print_sorted():
+    # sort by points
+    output_file = MagicMock()
+    results = {"A": 0, "B": 3}
+    ranking.print_sorted(output_file, results)
+    expected = [call("1. B, 3 pts\n"), call("2. A, 0 pts\n")]
+    assert expected == output_file.write.call_args_list,\
+        "Expected {} teams to be equal {} ".format(expected, output_file.write.call_args_list)
+
+    # test tie sorted alphabetically
+    output_file.reset_mock()
+    results = {"A": 0, "Z": 1, "D": 4, "F": 1}
+    ranking.print_sorted(output_file, results)
+    expected = [call("1. D, 4 pts\n"), call("2. F, 1 pt\n"), call("2. Z, 1 pt\n"),
+                call("4. A, 0 pts\n")]
+    assert expected == output_file.write.call_args_list, "Expected {} teams to be equal {} ".format(expected, output_file.write.call_args_list)
